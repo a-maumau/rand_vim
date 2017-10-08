a754dd1dd3680ea41274b78e2db73006a6efdd52
maumau <maumaumaumaumaumaumaumaumaumau@gmail.com>
2017-10-08 19:13:22 +0900

    update

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@
 cp ~/.bashrc ~/.bashrc.vim2emacs.back
 cp ./.rand_vim.sh ~/
 # please use ">>" not ">", I'm meaning it!
-echo "alias vim='~/.rand_vim.sh'" >> .bashrc
+echo "alias vim='~/.rand_vim.sh'" >> ~/.bashrc
+source ~/.bashrc
