4a1fd959e3368a3cb3f15fbaba8cd0d47dd10e32
maumau <maumaumaumaumaumaumaumaumaumau@gmail.com>
2017-12-22 02:21:55 +0900

    adapt to arguments

--- setup.sh
+++ setup.sh
@@ -2,6 +2,12 @@
 
 cp ~/.bashrc ~/.bashrc.vim2emacs.back
 cp ./.rand_vim.sh ~/
+chmod 755 ~/.rand_vim.sh
+
 # please use ">>" not ">"
-echo "alias vim='~/.rand_vim.sh'" >> ~/.bashrc
+echo ""                         >> ~/.bashrc
+echo "function vim() {"         >> ~/.bashrc
+echo "    ~/.rand_vim.sh \$@"   >> ~/.bashrc
+echo "}"                        >> ~/.bashrc
+
 source ~/.bashrc
