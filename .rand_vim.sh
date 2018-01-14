1cedcafc4df3fdfd2bca6aa4a44ae2fb9083d900
maumau <maumaumaumaumaumaumaumaumaumau@gmail.com>
2018-01-14 20:26:09 +0900

    fix

--- .rand_vim.sh
+++
@@ -1,6 +0,0 @@
-if [ $(($RANDOM%10)) -lt 2 ];
-then
-	emacs $@
-else 
-	vim $@
-fi
--- setup.sh
+++ setup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-cp ~/.bashrc ~/.bashrc.vim2emacs.back
-cp ./.rand_vim.sh ~/
-chmod 755 ~/.rand_vim.sh
+cp ~/.bashrc ~/.bashrc.not_vim_rand_original.back
 
 # please use ">>" not ">"
-echo ""                         >> ~/.bashrc
-echo "function vim() {"         >> ~/.bashrc
-echo "    ~/.rand_vim.sh \$@"   >> ~/.bashrc
-echo "}"                        >> ~/.bashrc
+echo ""                                  >> ~/.bashrc
+echo "function vim() {"                  >> ~/.bashrc
+echo "    if [ $(($RANDOM%10)) -lt 2 ];" >> ~/.bashrc
+echo "    then"                          >> ~/.bashrc
+echo "        emacs $@"                  >> ~/.bashrc
+echo "    else"                          >> ~/.bashrc
+echo "        vim $@"                    >> ~/.bashrc
+echo "    fi $@"                         >> ~/.bashrc
+echo "}"                                 >> ~/.bashrc
 
 source ~/.bashrc
