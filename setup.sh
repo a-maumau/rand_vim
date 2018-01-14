1626001a37e7ad85215a6555a2bcd69750757a88
maumau <maumaumaumaumaumaumaumaumaumau@gmail.com>
2018-01-14 20:47:24 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 cp ~/.bashrc ~/.bashrc.not_vim_rand_original.back
+cp ./.rand_vim.sh ~/
+chmod 755 ~/.rand_vim.sh
 
 # please use ">>" not ">"
-echo ""                                  >> ~/.bashrc
-echo "function vim() {"                  >> ~/.bashrc
-echo "    if [ $(($RANDOM%10)) -lt 2 ];" >> ~/.bashrc
-echo "    then"                          >> ~/.bashrc
-echo "        emacs $@"                  >> ~/.bashrc
-echo "    else"                          >> ~/.bashrc
-echo "        vim $@"                    >> ~/.bashrc
-echo "    fi $@"                         >> ~/.bashrc
-echo "}"                                 >> ~/.bashrc
+echo ""                       >> ~/.bashrc
+echo "function vim() {"       >> ~/.bashrc
+echo "    ~/.rand_vim.sh \$@" >> ~/.bashrc
+echo "}"                      >> ~/.bashrc
 
 source ~/.bashrc
